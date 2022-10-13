9f0c94989ff820fee454a2c478689fe70da454dc
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-10-13 10:00:01 +0600

    first

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage />,
+    loader: async () => {
+      return fetch(`https://jsonplaceholder.typicode.com/users`)
+    },
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
